721ec44058bbb514b0f79af2073184b1f6c27349
Marco Madera <marcoseguro2@gmail.com>
2024-04-11 16:03:05 -0700

    Des sacrifices (#7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig, passthroughImageService } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   image: {
     domains: ["images.unsplash.com"],
     service: passthroughImageService(),
   },
+  output: "server",
+  adapter: vercel(),
 });
