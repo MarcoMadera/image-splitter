fd3628a88b354e47259bc398175aa67d7f06e40c
MarcoMadera <marcoseguro2@gmail.com>
2024-04-11 06:23:02 -0700

    SKITZO

---
+++ src/types/image-splitter.ts
@@ -0,0 +1,23 @@
+export interface IUploadedImageState {
+  file: File | null;
+  name: string;
+  extension: string;
+}
+
+export interface IGetSplitImages {
+  gridX: number;
+  gridY: number;
+  uploadedImageState: IUploadedImageState;
+  target: string;
+}
+
+export interface IDrawGrid {
+  img: HTMLImageElement;
+  gridX: IGetSplitImages["gridX"];
+  gridY: IGetSplitImages["gridY"];
+}
+
+export interface IDownloadSplitImage {
+  outputName: string;
+  splitImages: HTMLCanvasElement[];
+}
