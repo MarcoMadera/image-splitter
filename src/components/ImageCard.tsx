dba82cdb9c3362687121eaf4736be10b278e6f5e
MarcoMadera <marcoseguro2@gmail.com>
2024-04-16 14:14:53 -0700

    hotline

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,88 @@
+import { type ReactElement, useState } from "react";
+
+import type { IUploadedImageState } from "types/image-splitter";
+import type { Image } from "types/imagesResponse";
+
+interface IImageCard {
+  image: Image;
+}
+
+function ImageCard({ image }: Readonly<IImageCard>): ReactElement {
+  const [hasFocus, setHasFocus] = useState<boolean>(false);
+
+  function handleImageClick() {
+    if (image.urls.regular) {
+      window.dispatchEvent(
+        new CustomEvent<IUploadedImageState>("updateImageState", {
+          detail: {
+            file: image.urls.regular,
+            name: image.slug,
+            extension: "",
+          },
+        })
+      );
+    }
+  }
+
+  return (
+    <button
+      key={image.id}
+      id={image.id}
+      draggable={true}
+      onDragStart={(e) => {
+        e.dataTransfer.setData("text/plain", image.id);
+      }}
+      onClick={() => {
+        handleImageClick();
+      }}
+      onFocus={(e) => {
+        e.currentTarget.classList.add("z-10", "scale-105", "shadow-lg");
+        setHasFocus(true);
+      }}
+      onBlur={(e) => {
+        e.currentTarget.classList.remove("z-10", "scale-105", "shadow-lg");
+        setHasFocus(false);
+      }}
+      title={`Select image to upload: ${image.alt_description}`}
+      className="group relative h-56 rounded shadow transition hover:z-10 hover:scale-125 hover:shadow-lg motion-reduce:hover:scale-105"
+    >
+      <img
+        src={image.urls.small_s3 ?? image.urls.small}
+        alt={image.alt_description}
+        className="h-full object-cover"
+        width={180}
+        height={120}
+        loading="eager"
+      />
+      <div
+        className={`${hasFocus ? "opacity-100" : "opacity-0"} absolute left-0 top-0 flex size-full flex-col items-start justify-end bg-black/25 p-2 transition-opacity group-hover:opacity-100`}
+      >
+        <p
+          className="mb-2 line-clamp-2 w-full justify-start text-ellipsis text-left text-xs text-white"
+          title={image.description ?? ""}
+        >
+          {image.description}
+        </p>
+        <a
+          href={image.user.links.html}
+          className="z-20 flex items-center text-left font-semibold text-white"
+          target="_blank"
+          rel="noreferrer"
+          title={image.user.first_name}
+        >
+          <img
+            src={image.user.profile_image.medium}
+            className="mr-1 inline-block size-8 rounded-full"
+            alt={image.user.first_name}
+            width={32}
+            height={32}
+            loading="eager"
+          />
+          <span>{image.user.first_name}</span>
+        </a>
+      </div>
+    </button>
+  );
+}
+
+export default ImageCard;
--- src/components/ImagesWrapper.tsx
+++ src/components/ImagesWrapper.tsx
@@ -1,8 +1,9 @@
 import { type PropsWithChildren, type ReactElement, useState } from "react";
 
-import type { IUploadedImageState } from "types/image-splitter";
-import type { Image, Images } from "types/imagesResponse";
+import type { Images } from "types/imagesResponse";
 import { getImages } from "utils/apiCalls";
+
+import ImageCard from "./ImageCard";
 
 interface IImagesWrapper {
   title: string;
@@ -35,20 +36,6 @@ function ImagesWrapper({
       .finally(() => setIsLoading(false));
   }
 
-  function handleImageClick(image: Image) {
-    if (image.urls.regular) {
-      window.dispatchEvent(
-        new CustomEvent<IUploadedImageState>("updateImageState", {
-          detail: {
-            file: image.urls.regular,
-            name: image.slug,
-            extension: "",
-          },
-        })
-      );
-    }
-  }
-
   return (
     <div className="container relative grid min-h-[39rem] min-w-full grid-cols-1 px-4 pb-1 pt-4">
       <h2 className="mb-1 text-2xl font-bold dark:text-neutral-50">{title}</h2>
@@ -57,54 +44,7 @@ function ImagesWrapper({
         className="flex flex-wrap justify-center gap-2 py-2"
       >
         {images?.length
-          ? images?.map((image) => (
-              <button
-                key={image.id}
-                id={image.id}
-                draggable={true}
-                onDragStart={(e) => {
-                  e.dataTransfer.setData("text/plain", image.id);
-                }}
-                onClick={() => {
-                  handleImageClick(image);
-                }}
-                className="group relative h-56 rounded shadow transition hover:z-10 hover:scale-125 hover:shadow-lg motion-reduce:hover:scale-105"
-              >
-                <img
-                  src={image.urls.small_s3 ?? image.urls.small}
-                  alt={image.alt_description}
-                  className="h-full object-cover"
-                  width={180}
-                  height={120}
-                  loading="eager"
-                />
-                <div className="absolute left-0 top-0 flex size-full flex-col items-start justify-end bg-black/25 p-2 opacity-0 transition-opacity group-hover:opacity-100">
-                  <p
-                    className="mb-2 line-clamp-2 w-full justify-start text-ellipsis text-left text-xs text-white"
-                    title={image.description ?? ""}
-                  >
-                    {image.description}
-                  </p>
-                  <a
-                    href={image.user.links.html}
-                    className="z-20 flex items-center text-left font-semibold text-white"
-                    target="_blank"
-                    rel="noreferrer"
-                    title={image.user.first_name}
-                  >
-                    <img
-                      src={image.user.profile_image.medium}
-                      className="mr-1 inline-block size-8 rounded-full"
-                      alt={image.user.first_name}
-                      width={32}
-                      height={32}
-                      loading="eager"
-                    />
-                    <span>{image.user.first_name}</span>
-                  </a>
-                </div>
-              </button>
-            ))
+          ? images?.map((image) => <ImageCard image={image} key={image.id} />)
           : children}
       </div>
 
