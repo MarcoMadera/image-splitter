94cb8a0aafbf4788ab1470b7cec53c30069b1279
Marco Madera <marcoseguro2@gmail.com>
2024-04-15 19:13:35 -0700

    Ripstick (#22)

--- src/components/ImagesWrapper.tsx
+++ src/components/ImagesWrapper.tsx
@@ -1,6 +1,7 @@
 import { type PropsWithChildren, type ReactElement, useState } from "react";
 
-import type { Images } from "types/imagesResponse";
+import type { IUploadedImageState } from "types/image-splitter";
+import type { Image, Images } from "types/imagesResponse";
 import { getImages } from "utils/apiCalls";
 
 interface IImagesWrapper {
@@ -34,8 +35,22 @@ function ImagesWrapper({
       .finally(() => setIsLoading(false));
   }
 
+  function handleImageClick(image: Image) {
+    if (image.urls.regular) {
+      window.dispatchEvent(
+        new CustomEvent<IUploadedImageState>("updateImageState", {
+          detail: {
+            file: image.urls.regular,
+            name: image.slug,
+            extension: "",
+          },
+        })
+      );
+    }
+  }
+
   return (
-    <div className="container relative grid min-h-[39rem] grid-cols-1 px-4 pb-1 pt-4">
+    <div className="container relative grid min-h-[39rem] min-w-full grid-cols-1 px-4 pb-1 pt-4">
       <h2 className="mb-1 text-2xl font-bold dark:text-neutral-50">{title}</h2>
       <div
         id="explore-image-container"
@@ -50,6 +65,9 @@ function ImagesWrapper({
                 onDragStart={(e) => {
                   e.dataTransfer.setData("text/plain", image.id);
                 }}
+                onClick={() => {
+                  handleImageClick(image);
+                }}
                 className="group relative h-56 rounded shadow transition hover:z-10 hover:scale-125 hover:shadow-lg motion-reduce:hover:scale-105"
               >
                 <img
@@ -62,25 +80,27 @@ function ImagesWrapper({
                 />
                 <div className="absolute left-0 top-0 flex size-full flex-col items-start justify-end bg-black/25 p-2 opacity-0 transition-opacity group-hover:opacity-100">
                   <p
-                    className="mb-2 line-clamp-2 justify-start text-ellipsis text-left text-xs text-white opacity-90"
+                    className="mb-2 line-clamp-2 justify-start text-ellipsis text-left text-xs text-white"
                     title={image.description ?? ""}
                   >
                     {image.description}
                   </p>
                   <a
                     href={image.user.links.html}
-                    className="z-20 font-semibold text-white"
+                    className="z-20 flex items-center text-left font-semibold text-white"
                     target="_blank"
+                    rel="noreferrer"
+                    title={image.user.first_name}
                   >
                     <img
                       src={image.user.profile_image.medium}
-                      className="mr-1 inline-block h-8 rounded-full"
+                      className="mr-1 inline-block size-8 rounded-full"
                       alt={image.user.first_name}
                       width={32}
                       height={32}
                       loading="eager"
                     />
-                    {image.user.first_name}
+                    <span>{image.user.first_name}</span>
                   </a>
                 </div>
               </button>
@@ -96,6 +116,7 @@ function ImagesWrapper({
           disabled={isLoading}
           type="button"
           aria-label="Reload images"
+          title="Reload images"
         >
           {reloadIcon ?? "Reload"}
         </button>
